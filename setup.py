c37b1cd976854a2f7679f966e4c06f4f14b62b2c
hemanggs <hemangmanhas@gmail.com>
2024-02-26 17:10:54 +0530

    minor

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
+ ## Building the App like a PyPi Package
+
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+
+EDITABLE_FLAG = '-e .' 
+
+def get_requirements(file_path:str)->List[str]: # arrow fn 
+    """
+    this function will a List of requirements ,as a list of strings
+    """
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n", "")  for req in requirements] # list comprenhension ,to edit list
+        
+        if EDITABLE_FLAG in requirements:
+            requirements.remove(EDITABLE_FLAG)
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
     return requirements
 
-setup(
-name='mlproject',
-version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
 
+setup(
+    name='First End to End',
+    version = '0.0.1',
+    author='hemang',
+    packages=find_packages(),
+    # install_requires=['pandas','numpy','seaborn']
+    install_requires = get_requirements('requirements.txt')
+    
+    
 )
